4bb49988447ddb2eb60ca9efa720296a32773fe1
Greyson <greyson.r.hamilton@gmail.com>
2020-11-07 18:31:07 -0500

    we shall begin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 import React from "react";
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+import * as yup from 'yup';
+import formSchema from './Components/formSchema';
+import Header from './Components/Header';
+import HomePage from './Components/HomePage';
+import Form from './Components/Form';
+
+const initialOrder = {
+
+  name: '',
+  size: '',
+  toppings: {
+    pepperoni: false,
+    sausage: false,
+    canadianbacon: false,
+    spicyitaliansausage: false,
+    grilledchicken: false,
+    onions: false,
+    greenPepper: false,
+    dicedtomato: false,
+    blackolives: false,
+    roastedgarlic: false,
+    artichokehearts: false,
+    threecheese: false,
+    pineapple: false,
+    xtracheese: false
+  },
+  specialInstructions: '',
+  quantity: '',
+};
 
 const App = () => {
+
+  
   return (
     <>
       <h1>Lambda Eats</h1>
